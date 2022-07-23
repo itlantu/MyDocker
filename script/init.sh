a42d4c25ac26d6a6bcb2ba8527077bbc951bec3f
itlantu <39409253+itlantu@users.noreply.github.com>
2022-07-23 20:31:03 +0800

    尝试将code-server设置为开机启动

--- script/init.sh
+++ script/init.sh
@@ -8,3 +8,7 @@ mkdir /home/coder
 # 将code-server作为守护进程
 # cp /home/MyDocker/script/code-server.service /etc/systemd/system/
 # systemctl start code-server.service
+
+# 将code-server设置为开机启动
+touch /etc/profile.d/code-server.open.sh
+cp /home/MyDocker/script/code-server.open.sh /etc/profile.d/
