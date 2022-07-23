2816b51ad054572e6761f705665e72063c4f31d4
itlantu <39409253+itlantu@users.noreply.github.com>
2022-07-23 20:59:23 +0800

    尝试让code-server成为守护进程

--- script/init.sh
+++ script/init.sh
@@ -6,9 +6,9 @@ touch ~/.config/code-server/config.yaml
 mkdir /home/coder
 
 # 将code-server作为守护进程
-# cp /home/MyDocker/script/code-server.service /etc/systemd/system/
-# systemctl start code-server.service
+cp /home/MyDocker/script/code-server.service /etc/systemd/system/
+systemctl start code-server.service
 
 # 将code-server设置为开机启动
-touch /etc/profile.d/code-server.open.sh
-cp /home/MyDocker/script/code-server.open.sh /etc/profile.d/
+# touch /etc/profile.d/code-server.open.sh
+# cp /home/MyDocker/script/code-server.open.sh /etc/profile.d/
