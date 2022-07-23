8af53ac16159227099ef40f9f022dc490ee09700
itlantu <39409253+itlantu@users.noreply.github.com>
2022-07-23 19:29:31 +0800

    修复了替换code-server配置文件失败的bug，并在apt下载vim时添加了-y参数

--- script/init.sh
+++ script/init.sh
@@ -1,10 +1,10 @@
 # 生成code-server配置文件
 touch ~/.config/code-server/config.yaml
-/cp /home/MyDocker/config.yaml ~/.config/code-server/config.yaml
+\cp /home/MyDocker/config.yaml ~/.config/code-server/config.yaml
 mkdir /home/coder
 
 # 将code-server作为守护进程
 su cp /home/MyDocker/script/code-server.service /etc/systemd/system/
 systemctl start code-server.service
 
-apt install vim
+apt install -y vim
